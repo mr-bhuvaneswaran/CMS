5bc669508499fdecad1833edd6b59727150fc058
mr.bhuvaneswaran <mr.bhuvaneswaran@gmail.com>
2017-07-07 21:14:50 +0530

    added some modules

--- src/app/app.js
+++ src/app/app.js
@@ -1,25 +1,25 @@
 'use strict';
 
-require('angular');
-require('angular-aria');
-require('angular-ui-router');
-require('angular-material');
-require('angular-animate');
-require('angular-resource');
-var moment = require('moment');
+// require('angular');
+// require('angular-aria');
+// require('angular-ui-router');
+// require('angular-material');
+// require('angular-animate');
+// require('angular-resource');
+// var moment = require('moment');
 require('./login/login-module.js');
 // require('angular-data-grid');
 // require('angular-sanitize');
 // require('sweetalert');
 // Declare app level module which depends on views, and components
 var myApp = angular.module('myApp', [
-  'ngMaterial',
+  // 'ngMaterial',
   'ngRoute',
   'myApp.login',
 ]);
 
 
-myApp.constant("moment",moment);
+// myApp.constant("moment",moment);
 myApp.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
