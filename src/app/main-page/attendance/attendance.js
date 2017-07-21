9a3039d91faa70cccb2109477dffa4d385a0719c
mr.bhuvaneswaran <mr.bhuvaneswaran@gmail.com>
2017-07-22 02:12:34 +0530

    dashboard annd attendance added

--- src/app/main-page/attendance/attendance.js
+++ src/app/main-page/attendance/attendance.js
@@ -3,8 +3,56 @@ function attendanceView(){
         restrict:'E',
         templateUrl:'app/main-page/attendance/attendance.html',
         controller:function(){
+        var attendance = document.getElementById("attendanceChart");
 
+var attendanceChart = new Chart(attendance, {
+          type: 'bar',
+          data:{
+          labels: ["June", "July", "August", "September"],
+          datasets: [{
+            data: [70, 80, 93.6, 79],
+            backgroundColor: 'rgb(121,134,203)',
+            borderWidth: 0
+        }]
+          },
+        options: {
+        showAllTooltips: true,
+		    showAllTooltips: true,
+		    tooltips: {
+		      custom: function(tooltip) {
+		        if (!tooltip) return;
+		        tooltip.displayColors = false;
+		      },
+		      callbacks: {
+		        label: function(tooltipItem, data) {
+		          return tooltipItem.xLabel + " : " + tooltipItem.yLabel + "%";
+		        },
+		        title: function(tooltipItem, data) {
+		          return;
+		        }
+          }
+        },
+        legend: {
+            display: false
+         },
+        scales: {
+            
+            yAxes: [{
+            ticks: {
+            
+                   min: 0,
+                   max: 100,
+                   callback: function(value){return value+ "%"}
+                },  
+								scaleLabel: {
+                   display: true,
+                   labelString: "Percentage"
+                }
+            }]
         }
+    }
+});
+}
     };
 }
 module.exports= attendanceView;
--- src/app/main-page/dashboard/dashboard.js
+++ src/app/main-page/dashboard/dashboard.js
@@ -3,6 +3,108 @@ function dashboardView(){
         restrict:'E',
         templateUrl:'app/main-page/dashboard/dashboard.html',
         controller:function(){
+var attendance = document.getElementById("attendanceChart");
+var gpa = document.getElementById("gpaChart");
+
+var myChart = new Chart(attendance, {
+    type: 'bar',
+
+   data:{
+   labels: ["June", "July", "August", "September"],
+        datasets: [{
+            data: [70, 80, 93.6, 79],
+            backgroundColor: 'rgb(121,134,203)',
+            borderWidth: 0
+        }]
+},
+    options: {
+        showAllTooltips: true,
+		    showAllTooltips: true,
+		    tooltips: {
+		      custom: function(tooltip) {
+		        if (!tooltip) return;
+		        tooltip.displayColors = false;
+		      },
+		      callbacks: {
+		        label: function(tooltipItem, data) {
+		          return tooltipItem.xLabel + " : " + tooltipItem.yLabel + "%";
+		        },
+		        title: function(tooltipItem, data) {
+		          return;
+		        }
+          }
+        },
+        legend: {
+            display: false
+         },
+        scales: {
+            
+            yAxes: [{
+            ticks: {
+            
+                   min: 0,
+                   max: 100,
+                   stepSize: 25,
+                   callback: function(value){return value+ "%"}
+                },  
+				   scaleLabel: {
+                   display: true,
+                   labelString: "Percentage"
+                }
+            }]
+        }
+    }
+});
+var gpaChart = new Chart(gpa, {
+          type: 'bar',
+          data:{
+          labels: ["1", "2", "3", "4"],
+          datasets: [{
+            data: [9.4, 8.8, 8.7, 8.6],
+            backgroundColor: 'rgb(121,134,203)',
+            borderWidth: 0
+        }]
+          },
+        options: {
+        showAllTooltips: true,
+		    showAllTooltips: true,
+		    tooltips: {
+		      custom: function(tooltip) {
+		        if (!tooltip) return;
+		        tooltip.displayColors = false;
+		      },
+		      callbacks: {
+		        label: function(tooltipItem, data) {
+		          return tooltipItem.yLabel;
+		        },
+		        title: function(tooltipItem, data) {
+		          return;
+		        }
+          }
+        },
+        legend: {
+            display: false
+         },
+        scales: {
+            
+            yAxes: [{
+            ticks: {
+            
+                   min: 0,
+                   max: 10,
+                   stepSize: 2,                   
+                   callback: function(value){return value}
+                },  
+				   scaleLabel: {
+                   display: true,
+                   labelString: "GPA"
+                }
+            }]
+        }
+    }
+});
+
+            
 
         }
     };
